63a432449fc0916b852868b14fe3407e1d1c546c
sk <sk@Ubuntu.myguest.virtualbox.org>
2024-01-16 16:31:53 +0530

    .

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,7 +1,1 @@
-PROJECT_BASE_SITE = $(call github,aesd-nettle,project_base)
-PROJECT_BASE_SITE_METHOD = git
-PROJECT_BASE_LICENSE = GPLv2
-PROJECT_BASE_LICENSE_FILES = COPYING
-PROJECT_BASE_DEPENDENCIES = host-pkgconf pkgconf
-PROJECT_BASE_INSTALL_STAGING = YES
-
+include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
